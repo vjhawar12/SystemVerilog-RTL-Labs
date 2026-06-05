e5c4bea4fc761482736b5b5505fc54431b5e0823
Vedant Jhawar <vedantj1203@gmail.com>
2026-06-05 07:15:52 -0400

    fixed oversampling in uart_rx and added tb_uart_rx

--- rtl/uart/uart_rx.sv
+++ rtl/uart/uart_rx.sv
@@ -17,6 +17,7 @@ IDLE       waiting for start bit
 START      validating/sampling start bit
 DATA       receiving 8 data bits
 STOP       checking stop bit
+DONE       data transaction done
 */
 
 typedef enum logic [2:0] {
@@ -30,15 +31,13 @@ typedef enum logic [2:0] {
 logic [DATA_FRAME_LENGTH - 1:0]shift_reg;
 state_t state;
 logic [3:0]counter;
-logic [3:0]tick_counter;
 logic [3:0]bit_count;
 
 always_ff @(posedge clk or negedge n_rst) begin 
     if (!n_rst) begin
         shift_reg <= '0;
-        counter = 0;
-        tick_counter = 0;
-        bit_count = 0;
+        counter <= 0;
+        bit_count <= 0;
         state <= IDLE;
     end else if (tick) begin
         case (state)
@@ -51,9 +50,9 @@ always_ff @(posedge clk or negedge n_rst) begin
                     end
             START : begin
                         counter <= counter + 1;
-                        if (counter == (OVERSAMPLE_RATE / 2) && rx_serial == 1'b0) begin
+                        if (counter == (OVERSAMPLE_RATE / 2)) begin
                             if (rx_serial == 1'b0) begin
-                                state <= DATA
+                                state <= DATA;
                             end else begin
                                 state <= IDLE;
                             end
@@ -62,16 +61,24 @@ always_ff @(posedge clk or negedge n_rst) begin
                         end
                     end
             DATA :  begin
-                        shift_reg[bit_count] <= rx_serial;
-                        if (bit_count < DATA_FRAME_LENGTH - 1) begin
-                            bit_count <= bit_count + 1;
-                        end else begin
-                            state <= STOP; 
+                        counter <= counter + 1;
+                        if (counter == OVERSAMPLE_RATE - 1) begin
+                            shift_reg[bit_count] <= rx_serial;
+                            if (bit_count < DATA_FRAME_LENGTH - 1) begin
+                                bit_count <= bit_count + 1;
+                            end else begin
+                                state <= STOP; 
+                            end
+                            counter <= 0;
                         end
                     end
             STOP :  begin
-                        if (rx_serial == 1'b1) begin
-                           state <= DONE;  
+                        counter <= counter + 1;
+                        if (counter == OVERSAMPLE_RATE - 1) begin 
+                            if (rx_serial == 1'b1) begin
+                                state <= DONE;  
+                            end
+                            counter <= 0;
                         end
                     end
             DONE : begin
---
+++ tb/uart/tb_uart_rx.sv
@@ -0,0 +1,99 @@
+`timescale 1ns/1ps
+
+module tb_uart_rx;
+
+    logic clk;
+    logic n_rst;
+    logic tick;
+    logic rx_serial;
+    localparam DATA_FRAME_LENGTH = 8;
+    localparam OVERSAMPLE_RATE = 16;
+    logic [DATA_FRAME_LENGTH - 1:0]data_out;
+    
+    uart_rx dut #(
+        .DATA_FRAME_LENGTH(DATA_FRAME_LENGTH),
+        .OVERSAMPLE_RATE(OVERSAMPLE_RATE)
+    )(
+        .clk(clk),
+        .n_rst(n_rst),
+        .tick(tick),
+        .rx_serial(rx_serial),
+        .data_out(data_out)
+    );
+
+    always #5 clk = ~clk;
+    assign tick = 1'b1;
+
+    task automatic send_one_bit(
+       input logic data_bit
+    );
+        begin
+            rx_serial = data_bit;
+            for (int i = 0; i < OVERSAMPLE_RATE; i++) begin
+                @(posedge clk);
+            end
+        end
+    endtask
+
+    task automatic send_one_byte (
+        input logic [DATA_FRAME_LENGTH - 1 : 0]data_frame
+    ); 
+        begin
+            // start bit
+            send_one_bit(0);
+            // data byte
+            for (int i = 0; i < DATA_FRAME_LENGTH; i++) begin
+                send_one_bit(data_frame[i]);
+            end
+            // stop bit
+            send_one_bit(1);
+        end
+
+    endtask
+
+    task automatic send_one_byte_test(
+        input logic [DATA_FRAME_LENGTH - 1 : 0]data_frame
+    ); 
+        begin
+            send_one_byte(data_frame);
+            @(posedge clk);
+            @(posedge clk);
+            @(posedge clk);
+            if (data_out != data_frame) begin
+                $fatal(1, "data_out != data_frame: Received: data_out=%0b Expected: data_out=%0b", data_out, data_frame);
+            end     
+        end
+
+    endtask
+
+    task automatic _reset();
+        begin
+            n_rst = 0;
+            #5;
+            n_rst = 1; 
+        end
+    endtask
+
+    initial begin
+        $dumpfile("build/uart_rx.vcd");
+        $dumpvars(0, tb_uart_rx);
+
+        clk       = 1'b0;
+        n_rst     = 1'b1;
+        rx_serial = 1'b1;
+
+        _reset();
+
+        logic [7:0]i;
+        for (i; i <= 0xFF; i++) begin
+            send_one_byte_test(i);
+            if (i == 0xFF) begin
+               break; 
+            end
+        end
+
+        $display("UART RX test passed.");
+        $finish;
+    end
+
+endmodule
